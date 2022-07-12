75c8a125aee6c96e861e2d0c2804d309721d16f2
ElAntoinoh <galopinantoine.contact@gmail.com>
2022-07-12 14:17:13 +0200

    Ajout d'exemples de cases

--- genererCase.py
+++ genererCase.py
@@ -2,7 +2,6 @@ import numpy as np
 import sys
 from random import randint
 from cv2 import imwrite
-import time
 
 # Création de l'image de base
 image = np.zeros((16, 16, 3), np.uint8)
@@ -15,7 +14,8 @@ b = int(sys.argv[3])
 # Variable désignant la variation maximale de la couleur de base 
 rng = 16
 
-def fonction():
+
+def genererCase():
     for x in range(16):
         for y in range(16):
             newColor = [r, g, b]
@@ -27,8 +27,10 @@ def fonction():
             
             image[x, y] = [newColor[0], newColor[1], newColor[2]]
 
-    time.sleep(1)
-    imwrite("casesGenerees/case.png", image)
 
-for i in range(5):
-    fonction()
+for i in range(4):
+    genererCase()
+
+    name = "cases/case" + str(i) + ".png"
+
+    imwrite(name, image)
